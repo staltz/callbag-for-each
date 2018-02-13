db01023fb71e2dd58c2d70f25deb57a87e9ac6ab
David Waller <david@krawaller.se>
2018-02-13 20:24:44 +0100

    test connecting sink to foreach

--- test.js
+++ test.js
@@ -108,3 +108,67 @@ test('it observes an async finite listenable source', t => {
   }, 700);
 });
 
+test('it passes data between source and a downstream sink', t => {
+  let history = [];
+  const report = (name,dir,t,d) => t !== 0 && d !== undefined && history.push([name,dir,t,d]);
+
+  const source = makeMockCallbag('source', report, true);
+  const middle = forEach(x => history.push(['middle', 1, x]));
+  const sink = makeMockCallbag('sink', report);
+
+  middle(source)(0, sink);
+
+  source.emit(1, 'foo');
+  source.emit(1, 'bar');
+  sink.emit(1, 'baz');
+  source.emit(2, 'errormsg');
+
+  t.deepEqual(history, [
+    ['middle', 1, 'foo'],
+    ['sink', 'fromUp', 1, 'foo'],
+    ['middle', 1, 'bar'],
+    ['sink', 'fromUp', 1, 'bar'],
+    ['sink', 'fromUp', 2, 'errormsg']
+  ], 'forEach passed data downstream as expected');
+
+  t.end();
+});
+
+test('it stops passing data on if sink terminates', t => {
+  let history = [];
+  const report = (name,dir,t,d) => t !== 0 && d !== undefined && history.push([name,dir,t,d]);
+
+  const source = makeMockCallbag('source', report, true);
+  const middle = forEach(x => history.push(['middle', 1, x]));
+  const sink = makeMockCallbag('sink', report);
+
+  middle(source)(0, sink);
+
+  source.emit(1, 'foo');
+  sink.emit(2);
+  source.emit(1, 'bar');
+
+  t.deepEqual(history, [
+    ['middle', 1, 'foo'],
+    ['sink', 'fromUp', 1, 'foo'],
+    ['middle', 1, 'bar'],
+  ], 'sink doesnt receive more messages after termination');
+
+  t.end();
+});
+
+function makeMockCallbag(name, report=()=>{}, isSource) {
+  let talkback;
+  let mock = (t, d) => {
+    if (t === 0){
+      talkback = d;
+      if (isSource) talkback(0, (st, sd) => report(name, 'fromDown', st, sd));
+    }
+    report(name, 'fromUp', t, d);
+  };
+  mock.emit = (t, d) => {
+    if (!talkback) throw new Error(`Can't emit from ${name} before anyone has connected`);
+    talkback(t, d);
+  };
+  return mock;
+}
